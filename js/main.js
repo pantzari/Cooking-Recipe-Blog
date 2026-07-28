bdaf4aa125ca9c996acd8e607cce2d3e0fed264f
kferousi <kferousi@threenitas.com>
2026-07-28 20:39:54 +0300

    burger menu completed

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const openBtn = document.getElementById("openMenu");
+const closeBtn = document.getElementById("closeMenu");
+const mobileMenu = document.getElementById("mobileMenu");
+
+openBtn?.addEventListener("click", () => {
+  mobileMenu.classList.add("is-open");
+  document.body.style.overflow = "hidden"; 
+});
+
+closeBtn?.addEventListener("click", () => {
+  mobileMenu.classList.remove("is-open");
+  document.body.style.overflow = "";
+});
+
+// close when clocking outside
+mobileMenu?.addEventListener("click", (e) => {
+  if (e.target === mobileMenu) {
+    mobileMenu.classList.remove("is-open");
+    document.body.style.overflow = "";
+  }
+});
